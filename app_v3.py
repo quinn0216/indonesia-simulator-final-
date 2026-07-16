6d74c8056188108049a668616565758651e21c99
quinn0216 <2510815@dtg.hs.kr>
2026-07-16 14:52:42 +0900

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -11,15 +11,96 @@ st.set_page_config(layout="wide")
 st.title("🇮🇩 인도네시아 주별 환경탄력성 지수 시뮬레이터")
 st.markdown("가중치를 조절하면 우측 지도의 주별 색상과 좌측 랭킹이 실시간으로 시각화됩니다.")
 
-# 주 이름을 깔끔하게 청소하는 도우미 함수 (줄 바꿈, 특수기호, 공백 전처리)
+# 🗺️ 엑셀 주 이름 -> GeoJSON 표준 주 이름(NAME_1) 매핑 딕셔너리
+PROVINCE_MAP = {
+    # 자카르타 / 요그야카르타
+    "dki jakarta": "Jakarta Raya",
+    "jakarta": "Jakarta Raya",
+    "jakarta raya": "Jakarta Raya",
+    "di yogyakarta": "Yogyakarta",
+    "yogyakarta": "Yogyakarta",
+    
+    # 아체 / 방카벨리퉁
+    "nanggroe aceh darussalam": "Aceh",
+    "aceh": "Aceh",
+    "bangka belitung": "Kepulauan Bangka Belitung",
+    "kepulauan bangka belitung": "Kepulauan Bangka Belitung",
+    "kep. bangka belitung": "Kepulauan Bangka Belitung",
+    
+    # 수마트라 지역
+    "west sumatera": "Sumatera Barat",
+    "sumatera barat": "Sumatera Barat",
+    "north sumatera": "Sumatera Utara",
+    "sumatera utara": "Sumatera Utara",
+    "south sumatera": "Sumatera Selatan",
+    "sumatera selatan": "Sumatera Selatan",
+    "kepulauan riau": "Kepulauan Riau",
+    "kep. riau": "Kepulauan Riau",
+    
+    # 자와 지역
+    "west java": "Jawa Barat",
+    "jawa barat": "Jawa Barat",
+    "east java": "Jawa Timur",
+    "jawa timur": "Jawa Timur",
+    "central java": "Jawa Tengah",
+    "jawa tengah": "Jawa Tengah",
+    "banten": "Banten",
+    
+    # 칼리만탄 지역
+    "west kalimantan": "Kalimantan Barat",
+    "kalimantan barat": "Kalimantan Barat",
+    "east kalimantan": "Kalimantan Timur",
+    "kalimantan timur": "Kalimantan Timur",
+    "south kalimantan": "Kalimantan Selatan",
+    "kalimantan selatan": "Kalimantan Selatan",
+    "central kalimantan": "Kalimantan Tengah",
+    "kalimantan tengah": "Kalimantan Tengah",
+    "north kalimantan": "Kalimantan Utara",
+    "kalimantan utara": "Kalimantan Utara",
+    
+    # 술라웨시 지역
+    "west sulawesi": "Sulawesi Barat",
+    "sulawesi barat": "Sulawesi Barat",
+    "north sulawesi": "Sulawesi Utara",
+    "sulawesi utara": "Sulawesi Utara",
+    "south sulawesi": "Sulawesi Selatan",
+    "sulawesi selatan": "Sulawesi Selatan",
+    "central sulawesi": "Sulawesi Tengah",
+    "sulawesi tengah": "Sulawesi Tengah",
+    "southeast sulawesi": "Sulawesi Tenggara",
+    "sulawesi tenggara": "Sulawesi Tenggara",
+    "gorontalo": "Gorontalo",
+    
+    # 누사텡가라 및 말루쿠 지역
+    "west nusa tenggara": "Nusa Tenggara Barat",
+    "nusa tenggara barat": "Nusa Tenggara Barat",
+    "east nusa tenggara": "Nusa Tenggara Timur",
+    "nusa tenggara timur": "Nusa Tenggara Timur",
+    "maluku": "Maluku",
+    "north maluku": "Maluku Utara",
+    "maluku utara": "Maluku Utara",
+    
+    # 파푸아 지역
+    "papua": "Papua",
+    "west papua": "Papua Barat",
+    "papua barat": "Papua Barat"
+}
+
+# 주 이름을 깔끔하게 청소하고 지도의 표준 명칭으로 교정하는 함수
 def clean_province_name(name):
     if pd.isna(name):
         return ""
-    # 문자열로 변환 후 줄 바꿈(\n)을 일반 공백으로 치환하고 양끝 공백 제거
+    # 줄 바꿈 제거 및 양끝 공백 정리
     name_str = str(name).replace('\n', ' ').strip()
-    # 연속된 공백 하나로 통일
     name_str = re.sub(r'\s+', ' ', name_str)
-    return name_str
+    
+    # 매핑 테이블 매칭 시도 (소문자 기준 검색)
+    lookup_key = name_str.lower()
+    if lookup_key in PROVINCE_MAP:
+        return PROVINCE_MAP[lookup_key]
+    
+    # 예외적인 한글 공백이나 대소문자 예방을 위한 타이틀화
+    return name_str.title()
 
 @st.cache_data
 def load_and_match_data():
@@ -55,14 +136,14 @@ def load_and_match_data():
     df_vars['GDP_diff'] = g2025 - g2007
     df_vars['Poverty_diff'] = p2025 - p2007
     
-    # 매칭용 조인 키 (공백 제거, 소문자화)
+    # 표준 매칭 키 생성
     df_temp['Join_Key'] = df_temp['Province'].str.replace(r'\s+', '', regex=True).str.lower()
     df_vars['Join_Key'] = df_vars['Province'].str.replace(r'\s+', '', regex=True).str.lower()
     
-    # 데이터 병합
+    # 데이터 병합 (알파벳 정렬 순서에 상관없이 Join_Key로 매칭)
     df_final = pd.merge(df_temp, df_vars[['Join_Key', 'GDP_diff', 'Poverty_diff']], on='Join_Key', how='inner')
     
-    # 노이즈 행 및 결측값 완벽 제거
+    # 결측치 및 노이즈 행 정리
     df_final = df_final[df_final['Province'].notna() & (df_final['Province'] != '')]
     df_final = df_final[~df_final['Province'].str.contains("total|average|합계|평균|province", case=False, na=False)]
     df_final = df_final.dropna(subset=['Temp_Change', 'GDP_diff', 'Poverty_diff']).reset_index(drop=True)
@@ -100,7 +181,8 @@ df_final['BCPI'] = (alpha * df_final['GDP_norm']) - (gamma * df_final['Poverty_n
 df_final['ETI'] = df_final['BCPI'] / (df_final['Temp_Change'].abs() + 1e-5)
 df_final['순위'] = df_final['ETI'].rank(ascending=False, method='min').astype(int)
 
-df_final['Geo_Province'] = df_final['Province'].astype(str).str.title().str.strip()
+# 지도의 NAME_1과 맞출 매핑 필드 추가
+df_final['Geo_Province'] = df_final['Province'].astype(str).str.strip()
 
 # 화면 분할 출력
 col1, col2 = st.columns([4, 6])
